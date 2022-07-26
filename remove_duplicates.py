edd66b657f715b0f85af2e3a23dddbae68673ece
Aiinane <videofunzone@gmail.com>
2022-07-26 04:18:59 +0300

    hm

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,4 +1,5 @@
 def remove_duplicates(array):
+    
     output_set = set()
     output_array = []
     i = 0
@@ -9,12 +10,8 @@ def remove_duplicates(array):
     for i in output_set:
         output_array.append(i)
 
- 
-            
-       
-
-             
     print (output_array)
+
 
 remove_duplicates([1,1,2]) # [1,2]
 remove_duplicates([0,0,1,1,1,2,2,3,3,4]) # [0,1,2,3,4]
