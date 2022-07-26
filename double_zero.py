815e1dbc22f520367e870c70d61c22b04c4141ef
Aiinane <videofunzone@gmail.com>
2022-07-26 04:23:06 +0300

    hm

--- double_zero.py
+++ double_zero.py
@@ -1,18 +1,35 @@
+
 def double_zero(array):
     output_array = []
     i = 0
     
-    
-    for i in range(len(array)): 
-        if array[i] == 0:
-            output_array.insert(i+2,array[i])
-            output_array.insert(i+2,array[i])
+    try:
+        for i in range(len(array)): 
+            if array[i] == 0:
+                output_array.insert(i+2,array[i])
+                output_array.insert(i+2,array[i])
 
-        else:
-            output_array.append(array[i]) 
+            else:
+                output_array.append(array[i]) 
 
-    print (output_array)
+        print (output_array)
+    except Exception as err:  # всі
+        print('Some err')
+
+try:
+    input = input().split(",")
+
+    array = []
+    for i in range(len(input)):
+        array.append(int(input[i]))
+
+    double_zero((array))
+except Exception as err:  # всі
+    print('Some err')
+
+
+     
     
-double_zero([1,0,2,3,0,4,5,0]) # [1,0,0,2,3,0,0,4,5,0,0]
-double_zero([1,2,0,3,4,0,5,0]) # [1,0,0,2,3,0,0,4,5,0,0]
-double_zero([1,2,3]) # [1,2,3] 
+# double_zero([1,0,2,3,0,4,5,0]) # [1,0,0,2,3,0,0,4,5,0,0]
+# double_zero([1,2,0,3,4,0,5,0]) # [1,0,0,2,3,0,0,4,5,0,0]
+# double_zero([1,2,3]) # [1,2,3] 
--- remove_duplicates.py
+++ remove_duplicates.py
@@ -3,16 +3,30 @@ def remove_duplicates(array):
     output_set = set()
     output_array = []
     i = 0
+    try:
+        for i in range(len(array)):
+            output_set.add(array[i])
 
-    for i in range(len(array)):
-        output_set.add(array[i])
+        for i in output_set:
+            output_array.append(i)
 
-    for i in output_set:
-        output_array.append(i)
+        print (output_array)
+    except Exception as err:  # всі
+        print('Some err')
 
-    print (output_array)
+try:
+    input = input().split(",")
+
+    array = []
+    for i in range(len(input)):
+        array.append(int(input[i]))
+   
+
+    remove_duplicates((array))
+except Exception as err:  # всі
+    print('Some err')
 
 
-remove_duplicates([1,1,2]) # [1,2]
-remove_duplicates([0,0,1,1,1,2,2,3,3,4]) # [0,1,2,3,4]
-remove_duplicates([1,1,1,1,1,1,1,1]) # [1]
+# remove_duplicates([1,1,2]) # [1,2]
+# remove_duplicates([0,0,1,1,1,2,2,3,3,4]) # [0,1,2,3,4]
+# remove_duplicates([1,1,1,1,1,1,1,1]) # [1]
